ab82edfb3f33a4af6f51df4c65c16284e58530e5
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-03-31 04:50:45 +0100

    datas

--- lib/data/coffee_data.dart
+++ lib/data/coffee_data.dart
@@ -1,7 +1,20 @@
 import 'package:coffee/models/Coffee.dart';
 
-final DUMMY_COFFEES = [Coffee(
-id: 'c1',
-name: 
-
-)];
+final DUMMY_COFFEES = [
+  Coffee(
+    id: 'c1',
+    name: 'Cappuciono',
+    description:
+        "this is an espresso whith a thick layer of faamy steamed milk",
+    price: 13,
+    imageUrl: "images/frappucino.png",
+  ),
+  Coffee(
+    id: 'c2',
+    name: 'Frapuciono',
+    description:
+        "this is an espresso whith a thick layer of faamy steamed milk",
+    price: 13,
+    imageUrl: "images/frappucino.png",
+  ),
+];
